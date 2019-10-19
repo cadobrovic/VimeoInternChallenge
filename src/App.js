fd7a5f253b7ac831383625a256850b3df03f3f5b
Karlo Dobrović <karlodobrovic@dyn-160-39-184-252.dyn.columbia.edu>
2019-10-19 12:23:41 -0400

    Initial commit. Some preliminry styling done to see how layout works on standard desktop.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="monsoon-container">
+        <img src="https://i.vimeocdn.com/video/595198868_505x160.jpg"/>
+        <div className="monsoon-text">
+          <h2>Monsoon III</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+            Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+            aliquam malesuada bibendum arcu vitae elementum curabitur. In 
+            iaculis nunc sed augue lacus viverra. Leo urna molestie at 
+            elementum eu facilisis sed odio. Dui id ornare arcu odio.
+          </p>
+        </div>
+      </div>
+
+      <div className="beams-container">
+        <div className="beams-text">
+          <h2>Beams</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+            Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+            aliquam malesuada bibendum arcu vitae elementum curabitur. In 
+            iaculis nunc sed augue lacus viverra. Leo urna molestie at 
+            elementum eu facilisis sed odio. Dui id ornare arcu odio.
+          </p>
+        </div>
+        <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg"/>
+
+      </div>
+
+      <div className="move2-container">
+
+      </div>
     </div>
   );
 }
