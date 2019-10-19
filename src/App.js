2514605e18374c5aae83f7dad2c0fe53f60102f6
Karlo Dobrović <karlodobrovic@dyn-160-39-184-252.dyn.columbia.edu>
2019-10-19 17:53:05 -0400

    Starting carousel. Created data and converted App to class.

--- src/App.js
+++ src/App.js
@@ -1,86 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import data from './data.js';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="monsoon-container">
-        <div className="monsoon-content">
-          <div className="monsoon-text">
-            <h2 className="monsoon-head">MONSOON III</h2>
-            <p className="monsoon-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
-              Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
-              aliquam curabitur. Vulputate mi sit amet mauris. 
-              Orci sagittis eu volutpat odio facilisis. Sapien nec 
-              curabitur. 
-            </p>
-          </div>
-          <img 
-            className="monsoon-img"
-            src="https://i.vimeocdn.com/video/595198868_505x160.jpg"
-          />
-
-        </div>
-        
-      </div>
-
-      <div className="bottom-container">
-
-        <div className="beams-container">
-
-          <div className="beams-content">
-
-            <div className="beams-text">
-              <h2 className="bottom-head">BEAMS</h2>
-              <p className="bottom-body">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: data.properties,
+      property: data.properties[0]
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="monsoon-container">
+          <div className="monsoon-content">
+            <div className="monsoon-text">
+              <h2 className="monsoon-head">MONSOON III</h2>
+              <p className="monsoon-body">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
                 Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
-                aliquam malesuada bibendum arcu vitae elementum curabitur. 
+                aliquam curabitur. Vulputate mi sit amet mauris. 
+                Orci sagittis eu volutpat odio facilisis. Sapien nec 
+                curabitur. 
               </p>
             </div>
             <img 
-              className="beams-img"
-              src="https://i.vimeocdn.com/video/589972810_530x315.jpg"
+              className="monsoon-img"
+              src="https://i.vimeocdn.com/video/595198868_505x160.jpg"
             />
-
+  
           </div>
-
           
-
         </div>
-
-        <div className="move2-container">
-          
-          <div className="move2-content">
-
-            
-            <div className="move2-text">
-              <h2 className="bottom-head">Move 2</h2>
-              <p className="bottom-body">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
-                Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
-                aliquam malesuada bibendum arcu vitae elementum curabitur. 
-              </p>
+  
+        <div className="bottom-container">
+  
+          <div className="beams-container">
+  
+            <div className="beams-content">
+  
+              <div className="beams-text">
+                <h2 className="bottom-head">BEAMS</h2>
+                <p className="bottom-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+                  Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+                  aliquam malesuada bibendum arcu vitae elementum curabitur. 
+                </p>
+              </div>
+              <img 
+                className="beams-img"
+                src="https://i.vimeocdn.com/video/589972810_530x315.jpg"
+              />
+  
             </div>
-            <img
-              className="move2-img" 
-              src="https://i.vimeocdn.com/video/590587169_530x315.jpg"
-            />
-
+  
+            
+  
           </div>
-          
+  
+          <div className="move2-container">
+            
+            <div className="move2-content">
+  
+              
+              <div className="move2-text">
+                <h2 className="bottom-head">Move 2</h2>
+                <p className="bottom-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                  Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+                  Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+                  aliquam malesuada bibendum arcu vitae elementum curabitur. 
+                </p>
+              </div>
+              <img
+                className="move2-img" 
+                src="https://i.vimeocdn.com/video/590587169_530x315.jpg"
+              />
+  
+            </div>
+            
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    "properties": [
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 0,
+        "title": "Hunt for the Wilderpeople",
+        "blurb": "Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows.",
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg",
+    },
+    {
+        "_id": "593e9297ec4cca9c56bf61af",
+        "index": 1,
+        "title": "Nichts passiert / A Decent Man",
+        "blurb": "A Swiss family takes a ski vacation and runs into trouble when the father, the titualr decent man, finds himself in a series of moral quandries.",
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",  
+    },
+    {
+        "_id": "593e929773c71925e5d7c11c",
+        "index": 2,
+        "title": "Vice Versa",
+        "blurb": "Come along with the Good Company crew as they travel throughout the US, Japan, BC and Quebec to showcase skiing in the best way possible.",
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",  
+    },
+    {
+        "_id": "593e92973d4d15eedb129aee",
+        "index": 3,
+        "title": "The Fourth Phase",
+        "blurb": "From the creators of The Art of FLIGHT, Red Bull Media House presents THE FOURTH PHASE, a snowboarding epic starring iconic athlete Travis Rice.",
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",  
+    },
+    {
+        "_id": "593e929728b7f8543b7907e1",
+        "index": 4,
+        "title": "Full Moon",
+        "blurb": "Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport.",
+        "picture": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",  
+    }
+    ]
+}
+
+export default data;
