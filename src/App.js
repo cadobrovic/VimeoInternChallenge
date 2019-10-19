a28ae5f48a21cc43e788db995d70156f593e90f4
Karlo Dobrović <karlodobrovic@dyn-160-39-184-252.dyn.columbia.edu>
2019-10-19 12:34:15 -0400

    Spacing of the bottom container is about right.

--- src/App.js
+++ src/App.js
@@ -21,36 +21,39 @@ function App() {
         </div>
       </div>
 
-      <div className="beams-container">
-        <div className="beams-text">
-          <h2>Beams</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
-            Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
-            aliquam malesuada bibendum arcu vitae elementum curabitur. In 
-            iaculis nunc sed augue lacus viverra. Leo urna molestie at 
-            elementum eu facilisis sed odio. Dui id ornare arcu odio.
-          </p>
+      <div className="bottom-container">
+
+        <div className="beams-container">
+          <div className="beams-text">
+            <h2>Beams</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+              Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+              aliquam malesuada bibendum arcu vitae elementum curabitur. In 
+              iaculis nunc sed augue lacus viverra. Leo urna molestie at 
+              elementum eu facilisis sed odio. Dui id ornare arcu odio.
+            </p>
+          </div>
+          <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg"/>
+
         </div>
-        <img src="https://i.vimeocdn.com/video/589972810_530x315.jpg"/>
 
-      </div>
-
-      <div className="move2-container">
-        <img src="https://i.vimeocdn.com/video/590587169_530x315.jpg"/>
-        <div className="move2-text">
-          <h2>Move 2</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
-            Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
-            aliquam malesuada bibendum arcu vitae elementum curabitur. In 
-            iaculis nunc sed augue lacus viverra. Leo urna molestie at 
-            elementum eu facilisis sed odio. Dui id ornare arcu odio.
-          </p>
+        <div className="move2-container">
+          <img src="https://i.vimeocdn.com/video/590587169_530x315.jpg"/>
+          <div className="move2-text">
+            <h2>Move 2</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ultrices dui sapien eget mi. Vulputate mi sit amet mauris. 
+              Orci sagittis eu volutpat odio facilisis. Sapien nec sagittis 
+              aliquam malesuada bibendum arcu vitae elementum curabitur. In 
+              iaculis nunc sed augue lacus viverra. Leo urna molestie at 
+              elementum eu facilisis sed odio. Dui id ornare arcu odio.
+            </p>
+          </div>
         </div>
       </div>
     </div>
