d72898a93792fec6d310a6cc30dba273a3a1f3ac
Karlo Dobrović <karlodobrovic@dyn-160-39-184-252.dyn.columbia.edu>
2019-10-20 01:17:32 -0400

    Carousel animates whew. Need to clean up styling and add button icons.

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Slide from './Slide';
 import './Carousel.css';
+import data from './data.js';
 
 class Carousel extends Component {
     constructor(props) {
@@ -27,6 +28,22 @@ class Carousel extends Component {
         }
     }
 
+    slideRight = () => {
+        if (this.state.position < 400) {
+          this.setState({
+            position: this.state.position + 100
+          });
+        }
+      }
+    
+      slideLeft = () => {
+        if (this.state.position >= 100) {
+          this.setState({
+            position: this.state.position - 100
+          });
+        }
+      }
+
 
 
     render() {
@@ -37,7 +54,7 @@ class Carousel extends Component {
                 <div className="row">
                     <button 
                         className="left pointer" 
-                        onClick={() => this.setImageState(-1)} 
+                        onClick={() => this.slideLeft()} 
                         href=""
                     >
                         {/* <i className="fas fa-chevron-left  fa-3x"></i> */}
@@ -46,22 +63,32 @@ class Carousel extends Component {
                     
                     <button 
                         className="right pointer" 
-                        onClick={() => this.setImageState(1)} 
+                        onClick={() => this.slideRight()} 
                         href="">
                             {/* <i className="fas fa-chevron-right fa-3x"></i> */}
                             Right
                     </button>
                 </div>
 
+                <div className="slide-wrap" style={{marginLeft: `-${this.state.position}%`}}>
                 {
-
+                    properties.map(property => 
+                    <Slide 
+                        image={property.image} 
+                        color={property.color} 
+                        title={property.title} 
+                        buy={property.buy}
+                        desc={property.desc}
+                        key={property._id} 
+                    />)
                 }
+                </div>
 
-                {this.state.imageState===0 && <Slide image="117162" color="3298DA" title="Hunt for the Wilderpeople" buy="85597" watch="182450021" desc="Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows."/>}
+                {/* {this.state.imageState===0 && <Slide image="117162" color="3298DA" title="Hunt for the Wilderpeople" buy="85597" watch="182450021" desc="Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We Do In The Shadows."/>}
                 {this.state.imageState===1 && <Slide image="112546" color="3298DA" title="Nichts passiert / A Decent Man" buy="82485" watch="178166421" desc="A Swiss family takes a ski vacation and runs into trouble when the father, the titular decent man, finds himself in a series of moral quandries."/>}
                 {this.state.imageState===2 && <Slide image="114877" color="715814" title="Vice Versa" buy="82432" watch="178080812" desc="Come along with the crew as they travel throughout the US, Japan, BC, and Quebec to showcase skiing in the best way possible."/>}
                 {this.state.imageState===3 && <Slide image="113412" color="3298DA" title="The Fourth Phase" buy="81262" watch="179075058" desc="From the creators of The Art of FLIGHT, Red Bull Media House presents THE FOURTH PHASE, a snowboarding epic starring iconic athlete Travis Rice."/>}
-                {this.state.imageState===4 && <Slide image="117464" color="000000" title="Full Moon" buy="85795" watch="180649691" desc="Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport."/>}
+                {this.state.imageState===4 && <Slide image="117464" color="000000" title="Full Moon" buy="85795" watch="180649691" desc="Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport."/>} */}
 
             </div>
         );
